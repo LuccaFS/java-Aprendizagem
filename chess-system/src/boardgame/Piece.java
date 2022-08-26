187903ab799c60b243cd783ebb64feff72645718
LuccaFS <flucca8@gmail.com>
2022-08-26 14:21:26 -0300

    Movimentos da Torre.

--- chess-system/src/boardgame/Piece.java
+++ chess-system/src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
     protected Position position;
     private Board board;
     
@@ -8,8 +8,26 @@ public class Piece {
         this.board = board;
     }
     
-    protected Board setBoard(){
+    protected Board getBoard(){
         return this.board;
+    }
+    
+    public abstract boolean[][] possibleMoves();
+    
+    public boolean possibleMove(Position pos){
+        return possibleMoves()[pos.getRow()][pos.getCol()];
+    }
+    
+    public boolean isThereAnyMove(){
+        boolean[][] mat = possibleMoves();
+        for(int i=0; i<mat.length; i++){
+            for(int j=0; j<mat.length; j++){
+                if(mat[i][j]){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
     
 }
--- chess-system/src/chess/ChessMatch.java
+++ chess-system/src/chess/ChessMatch.java
@@ -29,6 +29,7 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece)capturedPiece;
     }
@@ -42,7 +43,16 @@ public class ChessMatch {
     
     private void validateSourcePosition(Position pos){
         if(!board.thereIsAPiece(pos)){
-            throw new ChessException("There is no piece on source position");
+            throw new ChessException("There is no piece on source position.");
+        }
+        if(!board.piece(pos).isThereAnyMove()){
+            throw new ChessException("There is no possible movements for the chosen piece.");
+        }
+    }
+    
+    private void validateTargetPosition(Position source, Position target){
+        if(!board.piece(source).possibleMove(target)){
+            throw new ChessException("The chosen piece can't move to target position.");
         }
     }
     
