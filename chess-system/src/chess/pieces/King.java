e16f08c7162ecd881665bb2b2cb958dd9a853cac
LuccaFS <flucca8@gmail.com>
2022-08-26 15:57:51 -0300

    Movimentos do Rei.

--- chess-system/src/chess/pieces/King.java
+++ chess-system/src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -14,10 +15,28 @@ public class King extends ChessPiece {
     public String toString(){
         return "K";
     }
+    
+    private boolean canMove(Position pos){
+        ChessPiece p = (ChessPiece)getBoard().piece(pos);
+        return p == null || p.getColor() != getColor();
+    }
 
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getCols()];  
+        
+        Position p = new Position(0, 0);
+        
+        for(int i=(position.getRow()-1); i<=(position.getRow()+1); i++){
+            for(int j=(position.getCol()-1); j<=(position.getCol()+1); j++){
+                p.setValues(i, j);
+                if(p != position){
+                    if(getBoard().positionExists(p) && canMove(p)){
+                        mat[i][j] = true;
+                    }
+                }
+            }
+        }
         return mat;
     } 
     
