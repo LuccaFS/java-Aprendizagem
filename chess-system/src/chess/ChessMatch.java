10823e03437af3e36cb070048acca0712558084a
LuccaFS <flucca8@gmail.com>
2022-08-30 17:34:55 -0300

    Lógica Cavalo.

--- chess-system/src/chess/ChessMatch.java
+++ chess-system/src/chess/ChessMatch.java
@@ -166,6 +166,13 @@ public class ChessMatch {
         placeNewPiece('b', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('g', 8, new Bishop(board, Color.BLACK));
         
+        //knight
+        placeNewPiece('c', 1, new Knight(board, Color.WHITE));
+        placeNewPiece('f', 1, new Knight(board, Color.WHITE));
+        
+        placeNewPiece('c', 8, new Knight(board, Color.BLACK));
+        placeNewPiece('f', 8, new Knight(board, Color.BLACK));
+        
         //pawns
         for(int i = 0; i <8; i++){
             placeNewPiece((char)('a'+ i), 7, new Pawn(board, Color.BLACK));
